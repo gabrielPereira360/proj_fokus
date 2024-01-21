78b8d31a6a7167c65df99646b5b1c05e8ef202fa
Gabriel Pereira <gabriel.obom01@gmail.com>
2024-01-21 16:18:04 -0300

    Projeto Fokus Finalizado

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const html = document.querySelector('html'); // Captura os dados da tag html
-const btn_foco = document.querySelector('.app__card-button--foco'); // Captura os dados da tag botao de foco
-const btn_curto = document.querySelector('.app__card-button--curto'); // Captura os dados da tag botao de foco curto
-const btn_longo = document.querySelector('.app__card-button--longo'); // Captura os dados da tag botao de foco longo
-const banner = document.querySelector('.app__image'); // Captura a tag da foto do banner da página
-const texto_banner = document.querySelector('.app__title'); // Captura a tag do texto do cabecalho da página
-const botoes = document.querySelectorAll('.app__card-button'); // Captura os botoes de forma genérica
-const musica_chbx = document.querySelector('#alternar-musica'); // Obtem os dados da tag checkbox relacionada a música
-const musica = new Audio('./sons/luna-rise-part-one.mp3'); // Instancia uma música com o seu caminho 
-musica.loop = true; // Declaro que música irá sempre repetir e estará sempre tocando
-const btn_temporizador = document.querySelector('#start-pause'); // Captura a tag do botão do temporizador
-const iniciarOuPausarBtn = document.querySelector('#start-pause span'); // Captura o botão de começar
-const iniciarOuPausarImg = document.querySelector('.app__card-primary-butto-icon'); // Captura imagem do botao de pausar
-const tempoNaTela = document.querySelector('#timer'); // Captura a tag onde irá encaixar o tempo
+const html = document.querySelector('html'); 
+const btn_foco = document.querySelector('.app__card-button--foco'); 
+const btn_curto = document.querySelector('.app__card-button--curto');
+const btn_longo = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
+const texto_banner = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const musica_chbx = document.querySelector('#alternar-musica');
+const musica = new Audio('./sons/luna-rise-part-one.mp3');
+musica.loop = true;
+const btn_temporizador = document.querySelector('#start-pause');
+const iniciarOuPausarBtn = document.querySelector('#start-pause span');
+const iniciarOuPausarImg = document.querySelector('.app__card-primary-butto-icon');
+const tempoNaTela = document.querySelector('#timer'); 
 
 let tempoDecorridoEmSegundos = 1500; // Iniciar com 25 minutoss
 let intervaloId = null; 
@@ -57,7 +57,7 @@ btn_longo.addEventListener('click', () => {
 
 function alteraContexto (contexto){
 
-    botoes.forEach(function (btn){ // Percorre os botões e limpa os estilos
+    botoes.forEach(function (btn){ 
         btn.classList.remove('active');
     });
  
